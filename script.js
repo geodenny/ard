6ccb0d979468dfe4e9ca0ccb46d07d4ca3ace71b
geodenny <76803884+geodenny@users.noreply.github.com>
2023-04-24 01:09:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCtxXqkte_-ZtvWnyMGJFPQ0oFmfCHBQxI",
-    authDomain: "project2-55c6b.firebaseapp.com",
-    databaseURL: "https://project2-55c6b-default-rtdb.firebaseio.com",
-    projectId: "project2-55c6b",
-    storageBucket: "project2-55c6b.appspot.com",
-    messagingSenderId: "772092113396",
-    appId: "1:772092113396:web:ffc8ed77770d206dde49a1"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCtxXqkte_-ZtvWnyMGJFPQ0oFmfCHBQxI",
+  authDomain: "project2-55c6b.firebaseapp.com",
+  databaseURL: "https://project2-55c6b-default-rtdb.firebaseio.com",
+  projectId: "project2-55c6b",
+  storageBucket: "project2-55c6b.appspot.com",
+  messagingSenderId: "772092113396",
+  appId: "1:772092113396:web:ffc8ed77770d206dde49a1"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 $(document).ready(function(){
 	
